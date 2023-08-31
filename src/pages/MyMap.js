915434b7e75062f6f303e222c571a0f5d62865c9
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-08-31 12:29:43 +0200

    Rettet tilbage som det var før

--- src/pages/MyMap.js
+++ src/pages/MyMap.js
@@ -50,7 +50,7 @@ const MyMap = () => {
   return (
     <Map
       maplib={import("mapbox-gl")}
-      mapboxApiAccessToken ={process.env.REACT_APP_MAPBOX}
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
       style={{ width: "100vw", height: "50vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       initialViewState={{
