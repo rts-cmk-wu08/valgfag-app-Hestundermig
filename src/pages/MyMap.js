0232b0b6ac4a71d673c58f0e3dae0e20b9d772c8
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-08-31 12:47:12 +0200

    Tilbage til starten

--- src/pages/MyMap.js
+++ src/pages/MyMap.js
@@ -1,10 +1,10 @@
+/* eslint import/no-webpack-loader-syntax: off */
 
 import React, { useState, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import { Map, NavigationControl, Marker, GeolocateControl } from "react-map-gl";
 // import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 
-// eslint-disable import/no-webpack-loader-syntax
 mapboxgl.workerClass =
   require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
